0c2ac148c401300c766ddf4aeef8029aa3a7584b
MerkurevSergei <MerkurevSergei@yandex.ru>
2022-01-16 04:03:35 +0900

    Spring-JDBC-001. Initial module

---
+++ spring-jdbc/src/main/java/ru/merkurev/hibernate/training/spring/jdbc/springjdbc/SpringJdbcApplication.java
@@ -0,0 +1,13 @@
+package ru.merkurev.hibernate.training.spring.jdbc.springjdbc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringJdbcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringJdbcApplication.class, args);
+	}
+
+}
---
+++ spring-jdbc/src/test/java/ru/merkurev/hibernate/training/spring/jdbc/springjdbc/SpringJdbcApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.merkurev.hibernate.training.spring.jdbc.springjdbc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringJdbcApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
