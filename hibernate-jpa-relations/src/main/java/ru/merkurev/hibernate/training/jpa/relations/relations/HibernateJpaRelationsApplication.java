fcf5a38264e93d51411798ec52eb9e9ae0509de6
MerkurevSergei <MerkurevSergei@yandex.ru>
2022-01-24 02:13:32 +0900

    Hibernate-JPA-Relations-001. Init module

---
+++ hibernate-jpa-relations/src/main/java/ru/merkurev/hibernate/training/jpa/relations/relations/HibernateJpaRelationsApplication.java
@@ -0,0 +1,13 @@
+package ru.merkurev.hibernate.training.jpa.relations.relations;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HibernateJpaRelationsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HibernateJpaRelationsApplication.class, args);
+	}
+
+}
---
+++ hibernate-jpa-relations/src/test/java/ru/merkurev/hibernate/training/jpa/relations/relations/HibernateJpaRelationsApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.merkurev.hibernate.training.jpa.relations.relations;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HibernateJpaRelationsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
