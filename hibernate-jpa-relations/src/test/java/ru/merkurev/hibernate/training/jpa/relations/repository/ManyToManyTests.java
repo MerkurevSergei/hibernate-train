7057f49922e222be154c409c81d7e1ddf1f6d0b4
MerkurevSergei <MerkurevSergei@yandex.ru>
2022-02-03 02:35:30 +0900

    Hibernate-JPA-Relations-012. Avoid LazyInitializationException use Transactional

--- hibernate-jpa-relations/src/test/java/ru/merkurev/hibernate/training/jpa/relations/repository/ManyToManyTests.java
+++ hibernate-jpa-relations/src/test/java/ru/merkurev/hibernate/training/jpa/relations/repository/ManyToManyTests.java
@@ -7,6 +7,7 @@ import ru.merkurev.hibernate.training.jpa.relations.entity.Course;
 import ru.merkurev.hibernate.training.jpa.relations.entity.Review;
 import ru.merkurev.hibernate.training.jpa.relations.entity.Student;
 
+import javax.transaction.Transactional;
 import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.assertFalse;
@@ -18,10 +19,21 @@ class ManyToManyTests {
     @Autowired
     private CourseRepository courseRepository;
 
+    @Autowired
+    private StudentRepository studentRepository;
+
     @Test
     void getCourseWithStudents() {
         Course course = courseRepository.findById(1L);
         List<Student> students = course.getStudents();
         assertFalse(students.isEmpty());
+    }
+
+    @Test
+    @Transactional
+    void getCoursesFromStudentInTransaction() {
+        Student student = studentRepository.findById(1L);
+        List<Course> courses = student.getCourses();
+        assertFalse(courses.isEmpty());
     }
 }
