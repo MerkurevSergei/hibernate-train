923db9f4aa10cd67af9cd339cd00a96742e804bf
MerkurevSergei <MerkurevSergei@yandex.ru>
2022-02-05 03:55:49 +0900

    Hibernate-JPA-Inheritance-001. Init module

---
+++ hibernate-jpa-inheritance/src/main/java/ru/merkurev/hibernate/training/jpa/inheritance/HibernateJpaInheritanceApplication.java
@@ -0,0 +1,13 @@
+package ru.merkurev.hibernate.training.jpa.inheritance;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HibernateJpaInheritanceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HibernateJpaInheritanceApplication.class, args);
+	}
+
+}
---
+++ hibernate-jpa-inheritance/src/test/java/ru/merkurev/hibernate/training/jpa/inheritance/HibernateJpaInheritanceApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.merkurev.hibernate.training.jpa.inheritance;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HibernateJpaInheritanceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
