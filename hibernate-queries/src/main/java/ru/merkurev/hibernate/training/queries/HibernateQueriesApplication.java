cd2ee65a1f7846d908695ef91f97e7f6197255fa
MerkurevSergei <MerkurevSergei@yandex.ru>
2022-02-08 02:11:36 +0900

    Hibernate-Queries-001. Init module

---
+++ hibernate-queries/src/main/java/ru/merkurev/hibernate/training/queries/HibernateQueriesApplication.java
@@ -0,0 +1,13 @@
+package ru.merkurev.hibernate.training.queries;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HibernateQueriesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HibernateQueriesApplication.class, args);
+	}
+
+}
