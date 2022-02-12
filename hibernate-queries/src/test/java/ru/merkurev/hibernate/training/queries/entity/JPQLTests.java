f23be02618048b013ae5bd66e51b4d0b5cdc7e85
MerkurevSergei <MerkurevSergei@yandex.ru>
2022-02-13 02:09:41 +0900

    Hibernate-Queries-003. Select with grouping and ordering

--- hibernate-queries/src/test/java/ru/merkurev/hibernate/training/queries/entity/JPQLTests.java
+++ hibernate-queries/src/test/java/ru/merkurev/hibernate/training/queries/entity/JPQLTests.java
@@ -21,11 +21,29 @@ class JPQLTests {
 
     @Test
     @Transactional
-    void saveEmployee() {
+    void selectCoursesWithEmptyStudents() {
         TypedQuery<Course> query = em.createQuery("select c from Course c where c.students is empty", Course.class);
         List<Course> resultList = query.getResultList();
         assertFalse(resultList.isEmpty());
         assertTrue(resultList.get(0).getStudents().isEmpty());
+    }
+
+    @Test
+    @Transactional
+    void selectCoursesWithSomeStudents() {
+        TypedQuery<Course> query = em.createQuery("select c from Course c where size(c.students) >= 2", Course.class);
+        List<Course> resultList = query.getResultList();
+        assertFalse(resultList.isEmpty());
+        assertFalse(resultList.get(0).getStudents().isEmpty());
+    }
+
+    @Test
+    @Transactional
+    void selectCoursesOrderedByStudentsCount() {
+        TypedQuery<Course> query = em.createQuery("select c from Course c order by size(c.students)", Course.class);
+        List<Course> resultList = query.getResultList();
+        assertTrue(resultList.size() > 1);
+        assertTrue(resultList.get(0).getStudents().size() < resultList.get(1).getStudents().size());
     }
 
 }
