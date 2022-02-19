bb0bd306ba319d41f0bba60cb3566f387e691d6c
MerkurevSergei <MerkurevSergei@yandex.ru>
2022-02-20 03:21:35 +0900

    Hibernate-Queries-011. Criteria Query with Left Join

--- hibernate-queries/src/test/java/ru/merkurev/hibernate/training/queries/entity/CriteriaQueriesTests.java
+++ hibernate-queries/src/test/java/ru/merkurev/hibernate/training/queries/entity/CriteriaQueriesTests.java
@@ -8,13 +8,18 @@ import javax.persistence.EntityManager;
 import javax.persistence.TypedQuery;
 import javax.persistence.criteria.CriteriaBuilder;
 import javax.persistence.criteria.CriteriaQuery;
+import javax.persistence.criteria.Join;
+import javax.persistence.criteria.JoinType;
 import javax.persistence.criteria.Predicate;
 import javax.persistence.criteria.Root;
 import javax.transaction.Transactional;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertNotEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 @SpringBootTest
@@ -25,7 +30,6 @@ class CriteriaQueriesTests {
     private EntityManager em;
 
     @Test
-    @Transactional
     void criteriaBasic() {
         // Select c from Course c
 
@@ -48,7 +52,6 @@ class CriteriaQueriesTests {
     }
 
     @Test
-    @Transactional
     void criteriaLikeMega() {
         // Select c from Course c where c.name like "%mega%"
 
@@ -73,9 +76,8 @@ class CriteriaQueriesTests {
     }
 
     @Test
-    @Transactional
     void criteriaIsEmpty() {
-        // Select c from Course c where c.students is empty"
+        // Select c from Course c where c.students is empty
 
         // 1. Use Criteria Builder to create a Criteria Query returning the expected result object
         CriteriaBuilder cb = em.getCriteriaBuilder();
@@ -95,5 +97,30 @@ class CriteriaQueriesTests {
 
         List<Course> resultList = query.getResultList();
         assertEquals(1, resultList.size());
+    }
+
+    @Test
+    void criteriaLeftJoin() {
+        // Select c from Course c left join c.students s
+
+        // 1. Use Criteria Builder to create a Criteria Query returning the expected result object
+        CriteriaBuilder cb = em.getCriteriaBuilder();
+        CriteriaQuery<Course> cq = cb.createQuery(Course.class);
+
+        // 2. Define roots for tables which are involved in the query
+        Root<Course> root = cq.from(Course.class);
+
+        // 3. Define Predicates etc using CriteriaBuilder
+        Join<Object, Object> join = root.join("students", JoinType.LEFT);
+
+        // 4. Add Predicates etc to Criteria Query
+
+
+        // 5. Build the TypedQuery using the entity manager and criteria query
+        TypedQuery<Course> query = em.createQuery(cq.select(root));
+
+        List<Course> resultList = query.getResultList();
+        Set<Course> resultSet = new HashSet<>(resultList);
+        assertNotEquals(resultSet.size(), resultList.size());
     }
 }
